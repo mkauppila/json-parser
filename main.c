d5ede09bc1565b674e7ecc633d882df67563c13b
Markus Kauppila <markus.kauppila@gmail.com>
2021-03-07 11:13:57 +0200

    Clean up the code

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 typedef enum {
   json_number = 0,
   json_string = 1,
-  json_object = 3
+  json_object = 3,
 } json_type_t;
 
 struct json_t;
 void json_print(struct json_t *root);
 
-struct json_value_t { 
+struct json_value_t {
   struct json_value_t *next; // it's a linked list
   json_type_t type;
 
   char *name; // "name" of the property AKA key
-  
-  int value; // numeric value. int, decimal, boolean?
-  char  *string_value; // string value
-  struct json_t *json;// object or array
-};
 
+  int value;           // numeric value. int, decimal, boolean?
+  char *string_value;  // string value
+  struct json_t *json; // object or array
+};
 
 struct json_t {
   json_type_t type; // either object or array? Needs this because printing?
@@ -36,22 +35,19 @@ char *parse_key(char *const string, int *index) {
     // excepted " character
   }
   (*index)++;
-  
+
   int len = 0;
-  for (; *index < strlen(string); ++(*index)) { 
+  for (; *index < strlen(string); ++(*index)) {
     if (string[*index] == '"') {
       break;
     }
     len++;
   }
-  printf("key has lenght %d\n", len);
   char *key = malloc(len * sizeof(char)); // is there space for the '\0'? No??
   memcpy(key, (string + ((*index) - len)), len);
-  printf("key key %s\n", key);
 
   // consume the '"' character at the end
   if (string[*index] == '"') {
-    printf("handle ending \"." );
     (*index)++;
   }
 
@@ -66,23 +62,20 @@ char *parse_string_value(char *const string, int *index) {
 int strnum(char *s, int n) {
   int x = 0;
   int i = 0;
-  while(i < n) {
-      x = x * 10 + (s[i] - '0');
-      i++;
+  while (i < n) {
+    x = x * 10 + (s[i] - '0');
+    i++;
   }
   return x;
 }
 
-
 int parse_number_value(char *const string, int *index) {
   int len = 0;
-  for (; *index < strlen(string) && isdigit(string[*index]); ++(*index)) { 
+  for (; *index < strlen(string) && isdigit(string[*index]); ++(*index)) {
     len++;
   }
-  printf("its digits long %d", len);
 
   int number = strnum((string + ((*index) - len)), len);
-  printf("number is %d\n", number);
   return number;
 }
 
@@ -91,7 +84,7 @@ struct json_value_t *parse_key_value(char *const string, int *index) {
   value->next = NULL;
   (*index)++;
   value->name = parse_key(string, index);
-  // consume the ':' from the middle 
+  // consume the ':' from the middle
   // TODO: verify that it actually is ':'
 
   (*index)++;
@@ -103,13 +96,12 @@ struct json_value_t *parse_key_value(char *const string, int *index) {
     value->type = json_number;
   }
   // parse the value (in this case another string)
-    // if ? -> undefined? null?
-    // if { -> object
-    // if [ -> array
+  // if ? -> undefined? null?
+  // if { -> object
+  // if [ -> array
 
   return value;
 }
-
 
 struct json_t *json_parse(char *const string, int index) {
   struct json_t *node = malloc(sizeof(struct json_t));
@@ -119,12 +111,12 @@ struct json_t *json_parse(char *const string, int index) {
   for (int cursor = index; cursor < strlen(string); ++cursor) {
     char ch = string[cursor];
     if (ch == '{') {
-    int next = cursor + 1; // might go over strlen(string)
+      int next = cursor + 1; // might go over strlen(string)
 
       // parse all the key value pairs
       while (string[next] == '"' || string[next] == ',') {
+        // verify ',' and skip it
         if (string[next] == ',') {
-          // verify the ',' and skip
           cursor++;
         }
 
@@ -141,14 +133,10 @@ struct json_t *json_parse(char *const string, int index) {
         }
 
         next = cursor + 1;
-        // json_print(node);
       }
 
       if (string[cursor] == '}') {
-        printf("found closing }\n");
-        // loop through the values to append
-        // node->values = value;
-        break; // this has been parsed!
+        break;
       }
     } else if (ch == '[') {
       // parse array
@@ -171,19 +159,16 @@ void json_print(struct json_t *root) {
 }
 
 void json_free(struct json_t *root) {
-  // TODO: implement this properly, all the "nodes" 
+  // TODO: implement this properly, all the "nodes"
   // need to be freed
   free(root);
 }
 
-
 int main(int argc, char *argv[]) {
   struct json_t *json = json_parse("{\"msg-hello\":12345,\"foo\":\"bar\"}", 0);
-  // do someting
   printf("parsing done succesfully!\n");
   json_print(json);
 
-  // write custon
   json_free(json);
 
   return 0;
