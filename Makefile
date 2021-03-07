2eba13b590ea601e1857e00e1730fd44fe11cdbb
Markus Kauppila <markus.kauppila@gmail.com>
2021-03-07 17:27:40 +0200

    Split into multiple files

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-main: main.o 
-	$(CC) main.o -o main
+main: main.o json.o
+	$(CC) json.o main.o -o main
 
-test: test.o 
+test: test.o json.o
 	$(CC) test.o -o test
 
 main.o: main.c
@@ -9,6 +9,9 @@ main.o: main.c
 
 test.o: test.c
 	$(CC) -c test.c -o test.o
+
+json.o: json.c
+	$(CC) -c json.c -o json.o
 
 clean:
 	rm test 2> /dev/null
---
+++ json.h
@@ -0,0 +1,36 @@
+#ifndef JSON_H
+#define JSON_H
+
+#include <stdbool.h>
+
+typedef enum {
+  json_number = 0,
+  json_string,
+  json_boolean,
+  json_null,
+  json_object,
+  json_array,
+} json_type_t;
+
+struct json_value_t {
+  struct json_value_t *next;
+  json_type_t type;
+
+  char *name; // "name" of the property AKA key
+
+  int value;           // numeric value. int, decimal, boolean?
+  bool boolean_value;  // for boolean
+  char *string_value;  // string value
+  struct json_t *json; // object or array
+};
+
+struct json_t {
+  json_type_t type;
+  struct json_value_t *values; 
+};
+
+struct json_t *json_parse(char *const string, int index);
+void json_print(struct json_t *root);
+void json_free(struct json_t *root);
+
+#endif 
